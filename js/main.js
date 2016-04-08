f5ee11fbce1da2c87151913a1d041e525918620d
kristenfaulkner <faulkner.kristen@gmail.com>
2016-04-08 07:35:47 -0400

    wrote javascript to store and load contacts on myKin page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
-( function( $ ) {
-$( document ).ready(function() {
-$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
-	$('#cssmenu #menu-button').on('click', function(){
-		var menu = $(this).next('ul');
-		if (menu.hasClass('open')) {
-			menu.removeClass('open');
+// ( function( $ ) {
+// $( document ).ready(function() {
+// $('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+// 	$('#cssmenu #menu-button').on('click', function(){
+// 		var menu = $(this).next('ul');
+// 		if (menu.hasClass('open')) {
+// 			menu.removeClass('open');
+// 		}
+// 		else {
+// 			menu.addClass('open');
+// 		}
+// 	});
+// });
+// } )( jQuery );
+function setVariables() {
+		if(localStorage){
+
+			// If so, set local variables
+			if (!localStorage.contacts) {
+				var contactList = ["Kristen Faulkner", "Amelia Miller","Xinchi Dai",
+												"Joan Zhang", "Krzysztof Gajos", "Ofra Amir","Rakesh Khurana"];
+				localStorage.setItem('stickyData', contactList);
+			}
+
+		//  If local storage is not working, alert user
+		} else {
+			alert("Sorry, this app will not work because you do not have local storage!");
 		}
-		else {
-			menu.addClass('open');
-		}
-	});
+}
+function loadContacts() {
+	var contacts = localStorage.getItem('contacts').split(",");
+	for (i = 0; i < contacts.length; i++) {
+		$('#kinList').append('<li><a href="dataPage.html">' + contacts[i] + '</a></li>');
+	}
+}
+
+$(document).ready(function(){
+	setVariables();
+	loadContacts();
 });
-} )( jQuery );
