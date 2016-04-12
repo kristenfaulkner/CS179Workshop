78e120168761e89bd5cb5ac4826b808375416396
kristenfaulkner <faulkner.kristen@gmail.com>
2016-04-11 21:19:05 -0400

    testing again

--- hello.php
+++ hello.php
@@ -2,7 +2,8 @@
 <body>
 
 <p>Welcome <?php echo $_POST["firstName"]; ?><br></p>
-Your email address is: <?php echo $_POST["phoneNumber"]; ?>
+Your email address is: <?php echo $_POST["phoneNumber"]; ?><br>
+and the data is <? php echo $_POST; ?>
 
 </body>
 </html>
