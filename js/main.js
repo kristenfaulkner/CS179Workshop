6ec7f0c05a3f3701bcc2eb3129f1f4f4856bd7ab
Joan Zhang <detongzhang@Detongs-MacBook-Pro.local>
2016-04-20 19:58:49 -0400

    stop video

--- js/main.js
+++ js/main.js
@@ -155,6 +155,15 @@ function addActivity(){
 
 }
 
+unction toggleVideo(state) {
+    // if state == 'hide', hide. Else: show video
+    var div = document.getElementById("popupVid");
+    var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+    div.style.display = state == 'hide' ? 'none' : '';
+    func = state == 'hide' ? 'pauseVideo' : 'playVideo';
+    iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
+ 
+
 
 
 $(document).ready(function(){
