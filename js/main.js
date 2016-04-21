fddacea3f3e999e452319a72f3ca3c5ab5ac4804
Joan Zhang <detongzhang@Detongs-MacBook-Pro.local>
2016-04-20 20:06:32 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -156,14 +156,12 @@ function addActivity(){
 }
 
 function toggleVideo(state) {
-    // if state == 'hide', hide. Else: show video
     var div = document.getElementById("popupVid");
     var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
     div.style.display = state == 'hide' ? 'none' : '';
     func = state == 'hide' ? 'pauseVideo' : 'playVideo';
     iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
- 
-
+}
 
 
 $(document).ready(function(){
