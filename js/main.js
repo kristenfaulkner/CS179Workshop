44aee2a8b7c4f8d2507d136a44e0df6fcab2d384
xdai1091 <xdai1091@gmail.com>
2016-04-27 15:34:32 -0400

    revert again

--- js/main.js
+++ js/main.js
@@ -80,6 +80,7 @@ function pushActivity(ele){
 		
 		// Construct the string and push it to the localStroage
 		var result = "You sent a ".concat(new_activity.type," request to ",new_activity.name," on ",new_activity.month,"/",new_activity.date,"/",new_activity.year," ",new_activity.hour,":", new_activity.minute);
+ 		console.log(result);
  		var new_act = [{contents: result}];
  		for (i=0; i<activities.length; i++){
  			new_act.push(activities[i]);
@@ -120,6 +121,7 @@ function saveNewContact() {
 
 function saveGroupContact() {
 	var contacts = JSON.parse(localStorage.reminderList);
+		console.log("I made it here");
 	var newReminder = {
 		reminderID: contacts.length,
 		firstName: $("#groupname").val(),
@@ -191,6 +193,7 @@ function toggleVideo(state) {
     func = state == 'hide' ? 'pauseVideo' : 'playVideo';
     iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
 }
+
 
 $(document).ready(function(){
 		setVariables();
